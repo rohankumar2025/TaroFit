2436ab8b850eacc6f856e188ce8d6876357c4c9d
Rohan Kumar <rohankumar@ucsb.edu>
2023-03-09 15:30:30 -0800

    Initial Commit

---
+++ TaroFit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaroFit
+//
+//  Created by Rohan Kumar on 3/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaroFit/TaroFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaroFitApp.swift
+//  TaroFit
+//
+//  Created by Rohan Kumar on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TaroFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
