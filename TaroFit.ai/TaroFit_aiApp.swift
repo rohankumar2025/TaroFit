1565b4e646264081bc31d767c728635905f7b331
rohankumar2025 <rohankumar@ucsb.edu>
2023-07-18 11:09:29 -0700

    Initial Commit

---
+++ TaroFit.ai/TaroFit_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaroFit_aiApp.swift
+//  TaroFit.ai
+//
+//  Created by Rohan Kumar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TaroFit_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
